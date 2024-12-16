bd227c242aa03b55a78e661b4eb38905f1c074fa
Naomi Moreira <149202489+NamiMoreira@users.noreply.github.com>
2024-12-16 14:43:11 -0300

    Controller autenticação

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,10 @@ const router = Router();
 
 import {CreateUserController} from './controlers/user/CreateUserController'
 import {AuthUserController} from './controlers/user/AuthUserController'
+import { isAuthenticated } from './middlewares/isAuthenticated';
 
-router.post('/user', new CreateUserController().handle)
+
+router.post('/user',isAuthenticated, new CreateUserController().handle)
 
 router.post('/session', new AuthUserController().handle)
 
